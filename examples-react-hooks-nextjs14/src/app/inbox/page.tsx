9d08854ed4603603ca0be594b5a714abc3a84e57
Vinu Devaraj <vinu.devaraj@logidots.com>
2024-05-21 17:46:08 +0530

    feat: infinite scroll and blocks

--- examples-react-hooks-nextjs14/src/app/inbox/page.tsx
+++ examples-react-hooks-nextjs14/src/app/inbox/page.tsx
@@ -6,23 +6,24 @@ import React from 'react';
 import HomeComponent from '../../components/home';
 
 export default function Inbox() {
-  const apiKey = localStorage.getItem('apiKey');
-  const userId = localStorage.getItem('userId');
-  const baseUrl = localStorage.getItem('baseUrl');
+   const apiKey = localStorage.getItem('apiKey');
+   const userId = localStorage.getItem('userId');
+   const baseUrl = localStorage.getItem('baseUrl');
 
-  return (
-    <>
-      {baseUrl && userId && apiKey && (
-        <EngagespotProvider
-          options={{
-            baseUrl,
-            userId,
-            apiKey,
-          }}
-        >
-          <HomeComponent />
-        </EngagespotProvider>
-      )}
-    </>
-  );
+   return (
+      <>
+         {baseUrl && userId && apiKey && (
+            <EngagespotProvider
+               options={{
+                  baseUrl,
+                  userId,
+                  apiKey,
+                  itemsPerPage: 7,
+               }}
+            >
+               <HomeComponent />
+            </EngagespotProvider>
+         )}
+      </>
+   );
 }
